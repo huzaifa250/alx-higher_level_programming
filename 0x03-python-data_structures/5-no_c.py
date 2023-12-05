0716f3a8d3cb6490fffc9a8d924ade0d19492ded
huzaifa250 <huz.dark1@gmail.com>
2023-12-04 16:44:27 -0800

    edit task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     res = ""
-    for ch in my_string:
+    for ch in range(len(my_string)):
         if (my_string[ch] != 'c' and my_string[ch] != 'C'):
             res += my_string[ch]
     return res
