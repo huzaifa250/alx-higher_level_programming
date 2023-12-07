80c75e96f701fddea5039d53dc3a0064d0ae05ac
huzaifa250 <huz.dark1@gmail.com>
2023-12-07 07:30:56 -0800

    task 1 edit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,9 +6,9 @@ def search_replace(my_list, search, replace):
     of an element by another in a new list
     """
     new_list = []
-    for ele in my_list:
-        if elel == search:
+    for ele in range(len(my_list)):
+        if my_list[elel] == search:
             new_list.append(replace)
         else:
-            new_list.append(ele)
+            new_list.append(my_list[ele])
     return new_list
