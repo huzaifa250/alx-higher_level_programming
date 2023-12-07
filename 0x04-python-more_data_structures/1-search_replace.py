cb789190c6ed0ca8bd3150dd4af961357453213a
huzaifa250 <huz.dark1@gmail.com>
2023-12-07 07:28:15 -0800

    task 1 edit

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
+    """
+    Function that replaces occurrences
+    of an element by another in a new list
+    """
     new_list = []
     for ele in my_list:
         if elel == search:
