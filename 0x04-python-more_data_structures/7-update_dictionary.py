a5b731bf56ce429fec583eabd83364f547974b36
huzaifa250 <huz.dark1@gmail.com>
2023-12-08 14:38:38 -0800

    task 7,8

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    replace or add key/value in dictionary
+    """
+    a_dictionary[key] = value
+    return a_dictionary
