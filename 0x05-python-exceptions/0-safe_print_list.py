5b97c681149a77c5bfb059e0d721d5f781369b64
huzaifa250 <huz.dark1@gmail.com>
2023-12-18 17:17:54 -0800

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    s = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            s += 1
+        except IndexError:
+            break
+    print()
+    return s
