91e96e201f4244958f2536a24249c3f6afc3360d
huzaifa250 <huz.dark1@gmail.com>
2023-12-25 04:05:18 -0800

    edit task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" moduel defines class Square """
+
+
+class Square:
+    """ Repersent square class"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """get __size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set value of __size"""
+        if type(value) not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """calculates the square's area"""
+        return (self.__size) ** 2
