4c66eb368afb855b6615e6911440ea214b073ec5
huzaifa250 <huz.dark1@gmail.com>
2024-01-15 13:51:46 -0800

    add str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,8 @@ class Rectangle(Base):
         print('\n'*self.y, end='')
         for h in range(self.height):
             print(' '*self.x + '#'*self.width)
+
+    def __str__(self):
+        """Return str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.x, self.y, self.width, self.height)
