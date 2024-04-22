431b027bd4c129cc18494cb5be8b7cb05f1e529e
huzaifa250 <huz.dark1@gmail.com>
2024-04-22 15:34:03 -0700

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,3 +32,44 @@ class Square(Rectangle):
         """set the size instance attribute"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the Square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument  id attribute
+                - 2nd argument size attribute
+                - 3rd argument x attribute
+                - 4th argument y attribute
+            **kwargs (dict): New key,value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
