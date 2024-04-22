f4b9f06034c105f86e3d50f0aecb973fd6c4fe7e
huzaifa250 <huz.dark1@gmail.com>
2024-04-22 05:43:40 -0700

    add update with kwargs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,19 +118,35 @@ class Rectangle(Base):
             **kwargs (dict): New key/value pairs of attributes.
         """
         if args and len(args) != 0:
-            a = 0
+            i = 0
             for arg in args:
-                if a == 0:
+                if i == 0:
                     if arg is None:
                         self.__init__(self.width, self.height, self.x, self.y)
                     else:
                         self.id = arg
-                elif a == 1:
+                elif i == 1:
                     self.width = arg
-                elif a == 2:
+                elif i == 2:
                     self.height = arg
-                elif a == 3:
+                elif i == 3:
                     self.x = arg
-                elif a == 4:
+                elif i == 4:
                     self.y = arg
-                a += 1
+                i += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
