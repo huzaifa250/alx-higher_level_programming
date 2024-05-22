ba5d8d2d0e683605b20514fc3c8331ea3618c57b
huzaifa250 <huz.dark1@gmail.com>
2024-05-22 13:56:13 -0700

    task 1-3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- creates the table force_name on mysql server.
+-- If the table force_name already exists, script should not fail
+
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
