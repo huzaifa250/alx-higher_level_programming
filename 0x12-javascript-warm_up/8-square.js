3e9ba978187cd73d9329a7aec0fb329e1541b4c3
huzaifa250 <huz.dark1@gmail.com>
2024-06-20 23:05:41 +0200

    task 8-13

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*parses the third command-line argument as a number, 
+converts it to an integer using Math.floor */
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let ro = 0; ro < size; ro++) {
+    let row = '';
+    for (let i = 0; i < size; i++) row += 'X';
+    console.log(row);
+  }
+}
